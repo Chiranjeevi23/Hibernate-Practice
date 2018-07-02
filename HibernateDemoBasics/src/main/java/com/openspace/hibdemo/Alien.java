df9c90afb70e206e1c6f1a27531e9977d17ac26c
ChiranjeeviN <ChiranjeeviN@DESKTOP-0C8J1P3>
2018-07-02 18:20:57 +0530

    Added Annotations and practiced

--- HibernateDemoBasics/src/main/java/com/openspace/hibdemo/Alien.java
+++ HibernateDemoBasics/src/main/java/com/openspace/hibdemo/Alien.java
@@ -1,13 +1,19 @@
 package com.openspace.hibdemo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
+@Table(name="alien_table")
 public class Alien {
 	@Id
 	private int aid;
+	//@Transient //it will not be recorded in db when u run the program
 	private String aname;
+	@Column(name="alien_color") //you can specify colomn name
 	private String acolor;
 	public int getAid() {
 		return aid;
