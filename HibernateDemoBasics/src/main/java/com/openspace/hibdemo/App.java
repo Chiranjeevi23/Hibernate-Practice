00148e43e988155d5a5c0929fde0f875a09134a7
ChiranjeeviN <ChiranjeeviN@DESKTOP-0C8J1P3>
2018-07-02 17:59:47 +0530

    hibernate basics

---
+++ HibernateDemoBasics/src/main/java/com/openspace/hibdemo/App.java
@@ -0,0 +1,31 @@
+package com.openspace.hibdemo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Alien a = new Alien();
+        a.setAid(100);
+        a.setAname("Kratos");
+        a.setAcolor("White");
+        
+        Configuration cfg = new Configuration().configure().addAnnotatedClass(Alien.class);
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+        session.beginTransaction();
+        
+        session.save(a);
+        
+        session.getTransaction().commit();
+        
+        
+    }
+}
