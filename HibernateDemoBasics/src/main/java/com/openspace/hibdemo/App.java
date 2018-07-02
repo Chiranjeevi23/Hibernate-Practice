a819ac39678050142d892ef60153ff73d88f22fd
ChiranjeeviN <ChiranjeeviN@DESKTOP-0C8J1P3>
2018-07-02 18:34:30 +0530

    Fetching from the db

--- HibernateDemoBasics/src/main/java/com/openspace/hibdemo/App.java
+++ HibernateDemoBasics/src/main/java/com/openspace/hibdemo/App.java
@@ -2,26 +2,30 @@ package com.openspace.hibdemo;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Alien a = new Alien();
-        a.setAid(100);
-        a.setAname("Kratos");
-        a.setAcolor("White");
-        
-        Configuration cfg = new Configuration().configure().addAnnotatedClass(Alien.class);
-        SessionFactory sf = cfg.buildSessionFactory();
-        Session session = sf.openSession();
-        session.beginTransaction();
-        
-        session.save(a);
-        
-        session.getTransaction().commit();
-        
-        
-    }
+public class App {
+	public static void main(String[] args) {
+		Alien a = new Alien();
+		a.setAid(102);
+		a.setAname("hello");
+		a.setAcolor("crimson");
+
+		Configuration cfg = new Configuration().configure().addAnnotatedClass(Alien.class);
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session session = sf.openSession();
+
+		Transaction tx = session.beginTransaction();
+		//fetching
+		a = session.get(Alien.class, 100);
+		
+		//Saving
+		//session.save(a);
+
+		tx.commit();
+		
+		System.out.println(a);
+
+	}
 }
